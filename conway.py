b5811374536de39afebb5e96480b4c37196f49d5
sorapat-s <87912388+sorapat-s@users.noreply.github.com>
2021-08-23 17:22:01 +0700

    Update conway.py
    added pygame window

--- conway.py
+++ conway.py
@@ -1,50 +1,79 @@
 import numpy as np
+import pygame
+pygame.font.init()
+pygame.mixer.init()
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Conway's Game of Life")
+
+FPS = 60
+
+# drawingVariable
+MARGIN_WIDTH = 50
+MARGIN_HEIGHT = 50
+GRID_WIDTH = 100
+GRID_HEIGHT = 100
+BOARD_WIDTH = int(HEIGHT-MARGIN_HEIGHT-MARGIN_HEIGHT)
+BOARD_HEIGHT = int(HEIGHT-MARGIN_HEIGHT-MARGIN_HEIGHT)
+CELL_WIDTH = int(BOARD_WIDTH/GRID_WIDTH)
+CELL_HEIGHT = int(BOARD_HEIGHT/GRID_HEIGHT)
+
+#color
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 
 # gameVarible
 dieMin = 2
 dieMax = 3
 alive = 3
 gameRun = 1
-GRID_WIDTH = 20
-GRID_HEIGHT = 20
-run = 1
 
-board = np.zeros([GRID_WIDTH, GRID_HEIGHT], dtype = int)
-boardTemp = np.zeros([GRID_WIDTH, GRID_HEIGHT], dtype = int)
+def draw_window():
+    backBoard = pygame.Rect(MARGIN_HEIGHT-1, MARGIN_WIDTH-1, 402, 402)
+    pygame.draw.rect(WIN, WHITE, backBoard)
 
-def printBoard():
-    for i in range(GRID_WIDTH):
-        for j in range(GRID_HEIGHT):
-            if board[i][j] > 0:
-                print('O', end = '')
+    for x in range(0, BOARD_WIDTH, CELL_WIDTH):
+        for y in range(0, BOARD_HEIGHT, CELL_HEIGHT):
+            rect = pygame.Rect(x+MARGIN_WIDTH, y+MARGIN_HEIGHT, CELL_WIDTH, CELL_HEIGHT)
+            if board[int(x/CELL_WIDTH)][int(y/CELL_HEIGHT)] > 0:
+                pygame.draw.rect(WIN, WHITE, rect)
             else:
-                print(' ', end = '')
-        print('')
+                pygame.draw.rect(WIN, BLACK, rect)
+    
+    pygame.display.update()
 
-initialBoard = [[3,5],[3,6],[3,7],[4,7],[5,6]]
+def main():
+    clock = pygame.time.Clock()
+    run = True
+    global board, boardTemp
+    board = np.zeros([GRID_WIDTH, GRID_HEIGHT], dtype = int)
+    initialBoard = [[3,5],[3,6],[3,7],[4,7],[5,6]]
+    for i in range(len(initialBoard)):
+        board[initialBoard[i][0]][initialBoard[i][1]] = 1
 
-for i in range(len(initialBoard)):
-    board[initialBoard[i][0]][initialBoard[i][1]] = 1
-printBoard()
-if input("continue?\n") == 'n':
-    run = 0
+    while run:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
 
-while run:
-    for i in range(GRID_WIDTH):
-        for j in range(GRID_HEIGHT):
-            neighborSum = 0
-            for ii in range(i-1,i+2):
-                for jj in range(j-1,j+2):
-                    neighborSum += board[ii%GRID_WIDTH][jj%GRID_HEIGHT]>0
-            neighborSum -= board[i][j]>0
-            if neighborSum == alive:
-                boardTemp[i][j] = 1
-            if dieMin <= neighborSum <= dieMax and board[i][j] == 1:
-                boardTemp[i][j] = 1
-    board = boardTemp
-    boardTemp = np.zeros([GRID_WIDTH, GRID_HEIGHT], dtype = int)
-    printBoard()
-    if input("continue?\n") == 'n':
-        run = 0
+        draw_window()
+        boardTemp = np.zeros([GRID_WIDTH, GRID_HEIGHT], dtype = int)
+        for i in range(GRID_WIDTH):
+            for j in range(GRID_HEIGHT):
+                neighborSum = 0
+                for ii in range(i-1,i+2):
+                    for jj in range(j-1,j+2):
+                        neighborSum += board[ii%GRID_WIDTH][jj%GRID_HEIGHT]>0
+                neighborSum -= board[i][j]>0
+                if neighborSum == alive:
+                    boardTemp[i][j] = 1
+                if dieMin <= neighborSum <= dieMax and board[i][j] == 1:
+                    boardTemp[i][j] = 1
+        board = boardTemp
+        
 
-
+if __name__ == "__main__":
+    main()
